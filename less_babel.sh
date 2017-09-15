fa294e9288b1910fcaf9e0f08d351052c5edc399
yingjiechen <YingjieChen@live.cn>
2017-09-15 16:14:49 +0800

    Create less_babel.sh

---
+++ less_babel.sh
@@ -0,0 +1,10 @@
+for file in ./css/*.less;
+                do
+                        lessc -x $file -o ${file%".less"}.min.css;
+                done
+
+for file in ./js/*.jsx;
+                do
+                        babel $file --presets babel-preset-react -o ${file%".jsx"}.js;
+                        java -jar yuicompressor.jar  ${file%".jsx"}.js -o ${file%".jsx"}.min.js;
+                done
